90693ec7abf0500ee13cfee0efd92f5cca863973
Anthony Tsang <tsang@antho.com>
2021-07-01 00:15:09 +0900

    Fix required gems

--- lib/intrinio-realtime.rb
+++ lib/intrinio-realtime.rb
@@ -2,7 +2,7 @@ require 'logger'
 require 'uri'
 require 'http'
 require 'eventmachine'
-require 'websocket-client-simple'
+require 'websocket-eventmachine-client'
 
 module Intrinio
   module Realtime
